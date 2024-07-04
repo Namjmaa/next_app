4106cac607bd91c80f7070a351c0f165aad11d9b
Namjmaa <naagiidonama>
2024-07-04 15:15:10 +0800

    add functions

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,18 +1,29 @@
 //this is handler function for any requests coming in at the /api/post/ route
-import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
 import prisma from '../../../lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { authOptions } from '../auth/[...nextauth]';
+
 
 // POST /api/post
-export default async function handle(req, res) {
+export default async function handle(req:NextApiRequest, res: NextApiResponse) {
   const { title, content } = req.body;
 
-  const session = await getSession({ req });
-  const result = await prisma.post.create({
-    data: {
-      title: title,
-      content: content,
-      author: { connect: { email: session?.user?.email } },
-    },
-  });
-  res.json(result);
+  try {
+    console.log("cookie", req.headers.cookie)
+    const session = await getServerSession(req, res, authOptions);
+    
+    console.log('Session:', session);
+    //console.log(title, content)
+    const result = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        author: { connect: { email: session?.user?.email } },
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    console.log("GET SESSION:", error)
+  }
 }
